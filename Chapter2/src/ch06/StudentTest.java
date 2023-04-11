147a898ca064b15d4c682b287fffaa957e503c81
riz.dev <rizt.dev@gmail.com>
2023-04-11 20:21:24 +0900

    4/11

---
+++ Chapter2/src/ch06/StudentTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student();
+		studentLee.grade = 1;
+		
+		System.out.println(studentLee.showStudentInfo());
+
+		Student studentKim = new Student(123456, "Kim", 3);
+		System.out.println(studentKim.showStudentInfo());
+	}
+
+}
