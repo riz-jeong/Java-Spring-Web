ac45a969e232b6c64992cb67d3ca25b78fdafbaa
riz.dev <rizt.dev@gmail.com>
2023-05-23 17:54:42 +0900

    5/23

---
+++ Spring/ch3/src/main/java/com/spring/ch3/aop/MyMath.java
@@ -0,0 +1,19 @@
+package com.spring.ch3.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMath {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
